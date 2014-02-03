7d83b810d0ec86efaee9a5794636db10de28df54
Commun <bouteau.pierreolivier@gmail.com>
2014-02-03 16:43:08 +0100

    refactoring

---
+++ ModernUIApp1/ModernUIApp1/ModernUIApp1/Content/Search/SearchTable.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FirstFloor.ModernUI.Presentation;
+
+namespace ModernUIApp1.Content
+{
+    /// <summary>
+    /// Interaction logic for RechercheTable.xaml
+    /// </summary>
+    public partial class SearchTable : UserControl
+    {
+        public SearchTable()
+        {
+            InitializeComponent();
+
+            // Event manager for edit text year value
+            this.yearSlider.ValueChanged += yearSlider_ValueChanged;
+        }
+
+        private void yearSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            this.yearValue.Text = e.NewValue.ToString();
+        }
+    }
+}
