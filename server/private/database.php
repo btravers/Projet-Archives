59df9dd0f0cb6b6ecf0bd3b4ca58e60f0aef87f8
pierreolivier <bouteau.pierreolivier@gmail.com>
2014-02-21 09:16:51 +0100

    new database

---
+++ server/private/database.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * Database class
+ * SQL manager
+ */
+class Database
+{
+	/**
+	 * PDO
+	 */
+	private static $connection = NULL;
+
+	/**
+	 * Private function for the database connection
+	 */
+	private static function connect()
+	{
+		if(Database::$connection == NULL)
+		{
+			try
+			{
+				require_once("password.php");
+				Database::$connection = new PDO('mysql:host=localhost;dbname=' . Password::$dbname, Password::$username, Password::$password);
+			} catch(Exception $e) {
+				die("database error");
+			}
+		}
+	}
+
+	/**
+	 * Execute an update/insert query
+	 */
+	public static function exec($sql) 
+	{
+		Database::connect();
+
+		Database::$connection->exec($sql);
+	}
+	
+	/**
+	 * Execute a select query
+	 */
+	public static function query($sql)
+	{
+		Database::connect();
+
+		return Database::$connection->query($sql)->fetchAll();
+	}
+
+	/**
+	 * Check if the session id exists in database
+	 */
+	public static function isConnected($sessionId)
+	{
+
+	}
+}
