c6130e4a73141b1ff38836b6a322a914dcf892dc
Raphael Baron <raphael.baron@insa-rennes.fr>
2014-02-25 14:10:58 +0100

    Serveur : ajout de la classe Annotator

---
+++ server/private/helpers/annotator.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Authentificator class
+ * Authentificator helper
+ */
+class Annotator
+{
+	/**
+	 * Parse request and exec right function
+	 */
+	public static function exec($request)
+	{
+		$function = new ReflectionMethod(get_called_class(), $request->function);
+		return $function->invokeArgs(NULL, $request->arguments);
+	}
+
+	/**
+	 * Login
+	 */
+	public static function login($email, $password)
+	{
+		$passwordCrypt = sha1($password);
+
+		$result = Database::query("SELECT * FROM User WHERE email = '" . $email . "' AND password = '" . $passwordCrypt . "'");
+		if(count($result) > 0) {
+			$sessionId = sha1(microtime() . $email);
+			Database::exec("UPDATE User SET session_id = '" . $sessionId . "' WHERE id_user = " . $result[0]["id_user"]);
+			return array("message" => "connected", "session_id" => $sessionId);
+		} else {
+			return array("message" => "user_not_found");
+		}
+	}
+
+	/**
+	 * Transform url in php function
+	 */
+	public static function logout($sessionId)
+	{
+		$idUser = Database::getUser($sessionId);
+		if($idUser != -1) {
+			return array("message" => "disconnected");
+		} else {
+			return array("message" => "user_not_found");
+		}
+	}
+
+	/**
+	 * Exec the parsed request
+	 */
+	public static function register($email, $password)
+	{
+
+	}
+}
