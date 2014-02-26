e4ce03fc3cfe7962f7ccae150951e7a4ebada882
Commun <nicola.charpentier@orange.fr>
2014-02-26 11:38:09 +0100

    class connection

--- ModernUIApp1/ModernUIApp1/ModernUIApp1/Handlers/Utils/Connection.cs
+++ ModernUIApp1/ModernUIApp1/ModernUIApp1/Handlers/Utils/Connection.cs
@@ -8,57 +8,113 @@ using System.Threading.Tasks;
 
 namespace Handlers.Utils
 {
+    /* This class contains methods in order to connect the client to the web server */
     public class Connection
     {
         // Website, contains the root
-        private static String URL_ROOT = "http://www.google.fr/";
+        private static String ROOT_URL = "http://archipoilus.4estgump.org/";
 
-        public String getRequest(String request)
+        /* Encryption for URLs */
+        private static String URLEncryption(String url)
         {
-            WebRequest resultRequest = WebRequest.Create(URL_ROOT + request);
+            // TODO : Encryption here
+            url = url.Replace(" ", "+"); // For example
 
-            resultRequest.Method = "GET";
+            return url;
+        }
+        /* Encryption for POST arguments */
+        private static String ARGSEncryption(String arg)
+        {
+            // TODO : Encryption here
+            arg = arg.Replace(" ", "+"); // For example
 
-            return null;
+            return arg;
         }
 
-        public static String postRequest(String requestString)
+
+        /* Connection with GET method */
+        /* TODO : This method has to return a XML file */
+        public static String getRequest(String requestString)
         {
-            // Create a request using a URL that can receive a post. 
-            WebRequest request = WebRequest.Create("http://www.contoso.com/PostAccepter.aspx ");
-            // Set the Method property of the request to POST.
-            request.Method = "POST";
+            // Create a GET request
+            WebRequest resultRequest = WebRequest.Create(ROOT_URL + requestString);
+            resultRequest.Method = "GET";
+
+            // Get the response
+            WebResponse response = resultRequest.GetResponse();
+            
+            // DEBUG MODE Display
+            Console.WriteLine(((HttpWebResponse)response).StatusDescription);
+            
+            // Content send by the server
+            Stream dataStream = response.GetResponseStream();
+            // Read the content
+            StreamReader reader = new StreamReader(dataStream);
+            string responseFromServer = reader.ReadToEnd();
+
+            // DEBUG MODE Display the content.
+            Console.WriteLine(responseFromServer);
+            
+            // Clean up the streams
+            reader.Close();
+            dataStream.Close();
+            response.Close();
+
+            return responseFromServer;
+        }
+
+        /* Connection with POST method, accept arguments */
+        /* TODO : This method has to return a XML file */
+        public static String postRequest(String requestString, Dictionary<String, String> args)
+        {
+            // Create a POST request
+            WebRequest resultRequest = WebRequest.Create(ROOT_URL + requestString);
+            resultRequest.Method = "POST";
+
             // Create POST data and convert it to a byte array.
-            string postData = "This is a test that posts this string to a Web server.";
+            string postData = "";
+            
+            // Construct the data with args sent
+            foreach (String key in args.Keys.ToArray()) {
+                postData += ARGSEncryption(key) + "=" + ARGSEncryption(args[key]);
+            }
+            // UTF8 encoding
             byte[] byteArray = Encoding.UTF8.GetBytes(postData);
+
             // Set the ContentType property of the WebRequest.
-            request.ContentType = "application/x-www-form-urlencoded";
+            resultRequest.ContentType = "application/x-www-form-urlencoded";
+            
             // Set the ContentLength property of the WebRequest.
-            request.ContentLength = byteArray.Length;
+            resultRequest.ContentLength = byteArray.Length;
+            
             // Get the request stream.
-            Stream dataStream = request.GetRequestStream();
+            Stream dataStream = resultRequest.GetRequestStream();
             // Write the data to the request stream.
             dataStream.Write(byteArray, 0, byteArray.Length);
             // Close the Stream object.
             dataStream.Close();
-            // Get the response.
-            WebResponse response = request.GetResponse();
-            // Display the status.
+
+            // Get the response
+            WebResponse response = resultRequest.GetResponse();
+
+            // DEBUG MODE Display the status.
             Console.WriteLine(((HttpWebResponse)response).StatusDescription);
-            // Get the stream containing content returned by the server.
+
+            // Content send by the server
             dataStream = response.GetResponseStream();
-            // Open the stream using a StreamReader for easy access.
+            // Read the content
             StreamReader reader = new StreamReader(dataStream);
-            // Read the content.
             string responseFromServer = reader.ReadToEnd();
-            // Display the content.
+
+            // DEBUG MODE Display the content.
             Console.WriteLine(responseFromServer);
+
             // Clean up the streams.
             reader.Close();
             dataStream.Close();
             response.Close();
 
-            return null;
+            return responseFromServer;
         }
     }
 }
