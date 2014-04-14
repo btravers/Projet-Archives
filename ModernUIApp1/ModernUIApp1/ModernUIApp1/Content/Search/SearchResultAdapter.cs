b47d93f209f4028e84078caa8fd3fd8671a9c6e3
Pierre-Olivier Bouteau <bouteau.pierreolivier@gmail.com>
2014-04-14 23:42:37 +0200

    fix ListBox result

---
+++ ModernUIApp1/ModernUIApp1/ModernUIApp1/Content/Search/SearchResultAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace ModernUIApp1.Content.Search
+{
+    public class SearchResultAdapter
+    {
+        public string imagePath { get; private set; }
+        public string text { get; private set; }
+
+        public SearchResultAdapter(string imagePath, string text)
+        {
+            this.imagePath = imagePath;
+            this.text = text;
+        }
+    }
+}
--- ModernUIApp1/ModernUIApp1/ModernUIApp1/Content/Search/SearchResultViewModel.cs
+++ ModernUIApp1/ModernUIApp1/ModernUIApp1/Content/Search/SearchResultViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using FirstFloor.ModernUI.Presentation;
+using ModernUIApp1.Content.Search;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -14,43 +15,15 @@ namespace ModernUIApp1.Content
     /// </summary>
     public class SearchResultViewModel : NotifyPropertyChanged
     {
-        // 20 accent colors from Windows Phone 8
-        private Color[] accentImages = new Color[]{
-            Color.FromRgb(0xa4, 0xc4, 0x00),   // lime
-            Color.FromRgb(0x60, 0xa9, 0x17),   // green
-            Color.FromRgb(0x00, 0x8a, 0x00),   // emerald
-            Color.FromRgb(0x00, 0xab, 0xa9),   // teal
-            Color.FromRgb(0x1b, 0xa1, 0xe2),   // cyan
-            Color.FromRgb(0x00, 0x50, 0xef),   // cobalt
-            Color.FromRgb(0x6a, 0x00, 0xff),   // indigo
-            Color.FromRgb(0xaa, 0x00, 0xff),   // violet
-            Color.FromRgb(0xf4, 0x72, 0xd0),   // pink
-            Color.FromRgb(0xd8, 0x00, 0x73),   // magenta
-            Color.FromRgb(0xa2, 0x00, 0x25),   // crimson
-            Color.FromRgb(0xe5, 0x14, 0x00),   // red
-            Color.FromRgb(0xfa, 0x68, 0x00),   // orange
-            Color.FromRgb(0xf0, 0xa3, 0x0a),   // amber
-            Color.FromRgb(0xe3, 0xc8, 0x00),   // yellow
-            Color.FromRgb(0x82, 0x5a, 0x2c),   // brown
-            Color.FromRgb(0x6d, 0x87, 0x64),   // olive
-            Color.FromRgb(0x64, 0x76, 0x87),   // steel
-            Color.FromRgb(0x76, 0x60, 0x8a),   // mauve
-            Color.FromRgb(0x87, 0x79, 0x4e),   // taupe
-        };
-
-        private Color selectedAccentImage;
+        private List<SearchResultAdapter> results;
+        private SearchResultAdapter selectedResult;
 
         public SearchResultViewModel()
         {
- //           SyncThemeAndColor();
+            this.results = new List<SearchResultAdapter>();
 
- //           AppearanceManager.Current.PropertyChanged += OnAppearanceManagerPropertyChanged;
-        }
-
-        private void SyncThemeAndColor()
-        {
-            // and make sure accent color is up-to-date
-            //this.SelectedAccentImage = AppearanceManager.Current.AccentColor;
+            this.results.Add(new SearchResultAdapter("/Resources/fake_sheet.jpg", "test 1"));
+            this.results.Add(new SearchResultAdapter("/Resources/fake_sheet.jpg", "test 2"));
         }
 
         private void OnAppearanceManagerPropertyChanged(object sender, PropertyChangedEventArgs e)
@@ -61,23 +34,22 @@ namespace ModernUIApp1.Content
             }
         }
 
-        public Color[] AccentImages
+        public SearchResultAdapter[] ResultsList
         {
-            get { return this.accentImages; }
+            get { return this.results.ToArray(); }
         }
 
-
-        public Color SelectedAccentImage
+        public SearchResultAdapter SelectedResult
         {
-            get { return this.selectedAccentImage; }
+            get { return this.selectedResult; }
             set
             {
-                if (this.selectedAccentImage != value)
+                if (this.selectedResult != value)
                 {
-                    this.selectedAccentImage = value;
-                    OnPropertyChanged("SelectedAccentImage");
+                    this.selectedResult = value;
+                    OnPropertyChanged("SelectedResult");
 
-                    //AppearanceManager.Current.AccentColor = value;
+                    MainWindow.window.ContentSource = new Uri("/Pages/ViewTable.xaml", UriKind.Relative);
                 }
             }
         }
