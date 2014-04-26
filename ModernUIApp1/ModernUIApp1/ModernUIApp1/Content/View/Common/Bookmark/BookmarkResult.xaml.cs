ea6a186112882faa88ad2a48dafeb040fbe97cb7
nico <nicola.charpentier@orange.fr>
2014-04-26 13:55:11 +0200

    Client bookmark : popup is now in it's own class

--- ModernUIApp1/ModernUIApp1/ModernUIApp1/Content/View/Common/Bookmark/BookmarkResult.xaml.cs
+++ ModernUIApp1/ModernUIApp1/ModernUIApp1/Content/View/Common/Bookmark/BookmarkResult.xaml.cs
@@ -20,6 +20,7 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using FirstFloor.ModernUI.Windows.Controls;
 using FirstFloor.ModernUI.Presentation;
+using ModernUIApp1.Pages.Popups;
 
 namespace ModernUIApp1.Content.View.Common.Bookmark
 {
@@ -460,73 +461,4 @@ namespace ModernUIApp1.Content.View.Common.Bookmark
 
     }
 
-    /* Class popup for ModernDialog which returns a boolean result with show() method, useful class, could be use outside this class */
-    class Popup
-    {
-        ModernDialog pop;
-        public String result { get; protected set; }
-        bool textbox;
-        TextBox text;
-
-        public Popup(String title, String content, bool textbox)
-        {
-            this.textbox = textbox;
-
-            pop = new ModernDialog();
-            pop.Title = title;
-            
-            if (!textbox)
-            {
-                pop.Content = new Label().Content = content;
-            }
-            else
-            {
-                Grid grid = new Grid();
-                grid.Height = 75;
-                grid.Width = 250;
-
-                RowDefinition row1 = new RowDefinition();
-                row1.Height = GridLength.Auto;
-                RowDefinition row2 = new RowDefinition();
-                row2.Height = new GridLength(1, GridUnitType.Star);
-
-                grid.RowDefinitions.Add(row1);
-                grid.RowDefinitions.Add(row2);
-
-                Label lab = new Label();
-                lab.Content = content;
-                Grid.SetRow(lab, 0);
-
-                grid.Children.Add(lab);
-
-                text = new TextBox();
-                Grid.SetRow(text, 1);
-
-                grid.Children.Add(text);
-
-                pop.Content = grid;
-            }
-            
-            pop.FontSize = 24;
-            pop.OkButton.Click += OkButton_Click;
-            pop.Buttons = new Button[] { pop.OkButton, pop.CancelButton };
-        }
-
-        private void OkButton_Click(object sender, RoutedEventArgs e)
-        {
-            pop.DialogResult = true;
-
-            if (textbox && text != null)
-            {
-                result = text.Text;
-            }
-
-            pop.Close();
-        }
-
-        public bool show()
-        {
-            return (bool)pop.ShowDialog();
-        }
-    }
 }
---
+++ ModernUIApp1/ModernUIApp1/ModernUIApp1/Pages/Popups/Popup.cs
@@ -0,0 +1,82 @@
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ModernUIApp1.Pages.Popups
+{
+    /* Class popup for ModernDialog which returns a boolean result with show() method, useful class, could be use outside this class */
+    class Popup
+    {
+        ModernDialog pop;
+        public String result { get; protected set; } // Text return in the textbox
+        bool textbox;
+        TextBox text; 
+
+        /* Title, Label, textbox = true if you want a textbox */
+        public Popup(String title, String content, bool textbox)
+        {
+            this.textbox = textbox;
+
+            pop = new ModernDialog();
+            pop.Title = title;
+
+            if (!textbox)
+            {
+                pop.Content = new Label().Content = content;
+            }
+            else
+            {
+                Grid grid = new Grid();
+                grid.Height = 75;
+                grid.Width = 250;
+
+                RowDefinition row1 = new RowDefinition();
+                row1.Height = GridLength.Auto;
+                RowDefinition row2 = new RowDefinition();
+                row2.Height = new GridLength(1, GridUnitType.Star);
+
+                grid.RowDefinitions.Add(row1);
+                grid.RowDefinitions.Add(row2);
+
+                Label lab = new Label();
+                lab.Content = content;
+                Grid.SetRow(lab, 0);
+
+                grid.Children.Add(lab);
+
+                text = new TextBox();
+                Grid.SetRow(text, 1);
+
+                grid.Children.Add(text);
+
+                pop.Content = grid;
+            }
+
+            pop.FontSize = 24;
+            pop.OkButton.Click += OkButton_Click;
+            pop.Buttons = new Button[] { pop.OkButton, pop.CancelButton };
+        }
+
+        private void OkButton_Click(object sender, RoutedEventArgs e)
+        {
+            pop.DialogResult = true;
+
+            if (textbox && text != null)
+            {
+                result = text.Text;
+            }
+
+            pop.Close();
+        }
+
+        public bool show()
+        {
+            return (bool)pop.ShowDialog();
+        }
+    }
+}
